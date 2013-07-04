f7a1d4793a622c51d4a9992fb1db8cf529842c0d
suraj kumar <kumarsuraj512@gmail.com>
2013-07-05 00:47:56 +0530

    New python file is added

---
+++ information.py
@@ -0,0 +1,12 @@
+# this module give the information about  list,dict,tuples method etc 
+
+def info(object,spacing=10,collapse=1):
+    """ print methods and doc string.
+    Takes module,class,list,dictionary,or string."""
+    methodlist=[method for method in dir(object) if callable(getattr(object,method))]
+
+    
+    function=collapse and (lambda s: " ".join(s.split())) or (lambda s: s)
+    print "\n\n".join(["%s %s " %(method.ljust(spacing),function(str(getattr(object,method).__doc__))) for method in methodlist])
+
+
